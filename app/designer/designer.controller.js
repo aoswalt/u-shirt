4a262581b80f8f4d3737031c4d13c75f3f90f249
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-14 13:09:08 -0500

    color swatches with feedback

--- app/designer/designer.controller.js
+++ app/designer/designer.controller.js
@@ -1,10 +1,31 @@
 angular.module("ushirt")
   .controller("designCtrl", function(colorFactory) {
     const design = this;
+    design.isCollapsed = true;  //TODO(adam): specfic to each shape set
 
     design.shirtColorList = colorFactory.getShirtColorList();
-    design.shirtColor = design.shirtColorList[0];
     design.printColorList = colorFactory.getPrintColorList();
 
-    design.isCollapsed = true;
+    design.shirtColor = design.shirtColorList[0];
+    design.style = {
+      fill: Object.assign({}, design.printColorList[0]),
+      outline: Object.assign({}, design.printColorList[0]),
+      weight: 0
+    };
+
+    design.setShirtColor = color => {
+      design.shirtColor = color;
+      console.log("design.shirtColor", design.shirtColor);
+    };
+
+    design.setFillColor = color => {
+      Object.assign(design.style.fill, color);
+      console.log("design.style", design.style);
+    };
+
+    design.setOutlineColor = color => {
+      Object.assign(design.style.outline, color);
+      console.log("design.style", design.style);
+    };
+
   });

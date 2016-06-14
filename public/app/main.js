227aa3063b41ddba88028056887a4408b2dbab2c
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-13 20:47:33 -0500

    separate html via angular route

--- public/app/main.js
+++ public/app/main.js
@@ -1,4 +1,15 @@
-angular.module("ushirt", ["ui.bootstrap"])
+angular.module("ushirt", ["ngRoute", "ui.bootstrap"])
+  .config($routeProvider =>
+    $routeProvider
+      .when("/", {
+        controller: "designCtrl",
+        controllerAs: "design",
+        templateUrl: "app/designer/designer.html"
+      })
+      .otherwise("/")
+    )
+
+
   //TODO(adam): set page title based on view routing
   .controller("navCtrl", function() {
 

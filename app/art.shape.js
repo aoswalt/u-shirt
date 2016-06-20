d3b64570524b0e5034697b248ea9d2e12a70bd74
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-19 19:57:18 -0500

    extract shape drawing method to allow for implementing drawing thumbnails

--- app/art.shape.js
+++ app/art.shape.js
@@ -14,7 +14,43 @@ var Art = (function(art) {  // eslint-disable-line no-var
     this.finalTransform = Matrix.idmat.slice();
   };
 
-  art.Shape.drawShape = (shape, opts, tmat) => {
+
+  art.drawShape = shape =>
+    Art.Shape.drawShape(shape,
+      {fillStyle:"black", strokeStyle:"green", strokeWeight: 10}, Matrix.idmat);
+
+  art.drawThumb = (shape, ctx) =>
+    Art.Shape.drawThumb(shape,
+      {fillStyle:"black", strokeStyle:"green", strokeWeight: 10},
+      Matrix.idmat,
+      {width:100, height:100, pos:{x:25,y:25}},
+      ctx);
+
+
+  art.Shape.drawShape = (shape, opts, tmat) =>
+    drawShapeTo(shape, opts, tmat, art.ctx);
+
+  art.Shape.drawThumb = (shape, opts, tmat, env, ctx) => {
+    const thumbCanvas = ctx.canvas;
+    const shapeScale = 0.9;   // NOTE: leave room around for spacing
+    const canvasSize = thumbCanvas.width * shapeScale;
+    const offset = thumbCanvas.width * (1 - shapeScale) / 2;  // NOTE: mid
+    const ws = canvasSize / env.width;
+    const hs = canvasSize / env.height;
+    const scale = Math.min(ws, hs);
+    const wdiff = (canvasSize - env.width * scale) / 2;
+    const hdiff = (canvasSize - env.height * scale) / 2;
+
+    ctx.clearRect(0, 0, thumbCanvas.width, thumbCanvas.height);
+    ctx.save();
+    ctx.translate(-env.pos.x * scale + wdiff + offset,
+                  -env.pos.y * scale + hdiff + offset);
+    ctx.scale(scale, scale);
+    drawShapeTo(shape, opts, tmat, ctx);
+    ctx.restore();
+  };
+
+  const drawShapeTo = (shape, opts, tmat, ctx) => {
     let t = Matrix.idmat.slice();
     if(tmat) {
       t = Matrix.multmm(tmat, shape.finalTransform);
@@ -26,21 +62,17 @@ var Art = (function(art) {  // eslint-disable-line no-var
     });
 
     if(opts) {
-      art.ctx.fillStyle = opts.fillStyle;
+      ctx.fillStyle = opts.fillStyle;
       if(opts.fillStyle !== "none") {
-        art.ctx.fill();
+        ctx.fill();
       }
 
-      art.ctx.strokeStyle = opts.strokeStyle;
-      art.ctx.lineWidth = opts.strokeWeight;
+      ctx.strokeStyle = opts.strokeStyle;
+      ctx.lineWidth = opts.strokeWeight;
       if(opts.strokeStyle !== "none" && opts.strokeWeight !== 0) {
-        art.ctx.stroke();
+        ctx.stroke();
       }
     }
-  };
-
-  art.Shape.drawThumb = (shape, opts, tmat, ctx) => {
-
   };
 
   art.Shape.addTransform = (shape, tmat) => {

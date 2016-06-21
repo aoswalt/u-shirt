6e38e11941dbe2f869b56a4d9e27ba6e6b8bdabe
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-21 13:20:28 -0500

    sass setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,33 @@ const gulp = require("gulp");
 const connect = require("gulp-connect");
 const del = require("del");
 const runSequence = require("run-sequence");
+const sass = require("gulp-sass");
 
 const sourcePath = "./src";
 const publicPath = "./public";
+const sassRelativePath = "/styles";
 
 const allSourcePath = `${sourcePath}/**/*`;
 const allPublicPath = `${publicPath}/**/*`;
+const allSassPath = `${sourcePath}${sassRelativePath}/**/*.scss`;
+const sassEntryPath = `${sourcePath}${sassRelativePath}/main.scss`;
+const staticPath = [allSourcePath, `!${allSassPath}`];
 
 
 gulp.task("clean:all", () => del(allPublicPath));
 
+
 gulp.task("static:copy", () =>
-  gulp.src(allSourcePath)
+  gulp.src(staticPath)
     .pipe(gulp.dest(publicPath)));
 
-gulp.task("static:watch", () =>
-  gulp.watch(allSourcePath, ["static:copy"]));
+
+gulp.task("sass:compile", () => (
+  gulp.src(sassEntryPath)
+    .pipe(sass())
+      .on("error", sass.logError)
+    .pipe(gulp.dest(`${publicPath}/styles`))
+));
 
 
 gulp.task("connect", () =>
@@ -32,11 +43,6 @@ gulp.task("livereload", () =>
   gulp.src(allPublicPath)
     .pipe(connect.reload()));
 
-gulp.task("livereload:watch", () =>
-  gulp.watch(allPublicPath, ["livereload"]));
-
-
-gulp.task("build", ["static:copy"]);
 
 gulp.task("watch", () =>
   runSequence(
@@ -44,7 +50,20 @@ gulp.task("watch", () =>
     "build",
     [
       "static:watch",
+      "sass:watch",
       "livereload:watch",
       "connect"
     ]
-));
+  ));
+
+gulp.task("static:watch", () =>
+  gulp.watch(staticPath, ["static:copy"]));
+
+gulp.task("sass:watch", () =>
+  gulp.watch(allSassPath, ["sass:compile"]));
+
+gulp.task("livereload:watch", () =>
+  gulp.watch(allPublicPath, ["livereload"]));
+
+
+gulp.task("build", ["sass:compile", "static:copy"]);
